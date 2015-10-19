96ac5244c9d3bfa891409d477addc2f76ef2e893
Artiomtb <artiomtb@gmail.com>
2015-10-19 12:16:23 +0300

    Added methods to get available values for point

--- src/main/java/com/artiomtb/sudokuresolver/SudokuField.java
+++ src/main/java/com/artiomtb/sudokuresolver/SudokuField.java
@@ -4,7 +4,9 @@ import com.artiomtb.sudokuresolver.exceptions.IncorrectSudokuFieldLineNumberExce
 import com.artiomtb.sudokuresolver.exceptions.IncorrectSudokuPointException;
 import org.apache.log4j.Logger;
 
+import java.util.ArrayList;
 import java.util.BitSet;
+import java.util.List;
 
 public class SudokuField {
 
@@ -19,7 +21,7 @@ public class SudokuField {
 
     public SudokuField(Iterable<SudokuPoint> points) throws IncorrectSudokuPointException {
         this.field = getEmptyField();
-        for(SudokuPoint point: points) {
+        for (SudokuPoint point : points) {
             this.field[point.getPosX() - 1][point.getPosY() - 1] = point;
         }
         LOG.debug("Created: " + toString());
@@ -123,6 +125,131 @@ public class SudokuField {
     public void setPoint(int posX, int posY, int value) throws IncorrectSudokuPointException {
         SudokuPoint point = new SudokuPoint(posX, posY, value);
         setPoint(point);
+    }
+
+    private int getSquareByPos(int posX, int posY) {
+        int squareNum;
+        if (posX < 4) {
+            if (posY < 4) {
+                squareNum = 1;
+            } else if (posY < 7) {
+                squareNum = 4;
+            } else {
+                squareNum = 7;
+            }
+        } else if (posX < 7) {
+            if (posY < 4) {
+                squareNum = 2;
+            } else if (posY < 7) {
+                squareNum = 5;
+            } else {
+                squareNum = 8;
+            }
+        } else {
+            if (posY < 4) {
+                squareNum = 3;
+            } else if (posY < 7) {
+                squareNum = 6;
+            } else {
+                squareNum = 9;
+            }
+        }
+        return squareNum;
+    }
+
+    public List<Integer> getAvailableValuesForPoint(int posX, int posY) throws IncorrectSudokuFieldLineNumberException {
+        List<Integer> nonAvailableValuesVertical = getNonAvailableValuesForVertical(posX);
+        List<Integer> nonAvailableValuesHorizontal = getNonAvailableValuesForHorizontal(posY);
+        List<Integer> nonAvailableValuesSquare = getNonAvailableValuesForSquare(getSquareByPos(posX, posY));
+        List<Integer> nonAvailableValues = new ArrayList<>();
+        List<Integer> availableValues = new ArrayList<>();
+        nonAvailableValues.addAll(nonAvailableValuesHorizontal);
+        nonAvailableValues.removeAll(nonAvailableValuesVertical);
+        nonAvailableValues.addAll(nonAvailableValuesVertical);
+        nonAvailableValues.removeAll(nonAvailableValuesSquare);
+        nonAvailableValues.addAll(nonAvailableValuesSquare);
+        LOG.debug("Non available values for point " + nonAvailableValues);
+        for (int i = 1; i <= 9; i++) {
+            if (!nonAvailableValues.contains(i)) {
+                availableValues.add(i);
+            }
+        }
+        LOG.debug("Available values for point: " + availableValues);
+        return availableValues;
+    }
+
+    public List<Integer> getAvailableValuesForPoint(SudokuPoint point) {
+        List<Integer> availableValues = null;
+        try {
+            availableValues = getAvailableValuesForPoint(point.getPosX(), point.getPosY());
+        } catch (IncorrectSudokuFieldLineNumberException e) {
+            LOG.error("Exception while getting available values for " + point, e);
+        }
+        return availableValues;
+    }
+
+    private List<Integer> getNonAvailableValuesForVertical(int lineNum) throws IncorrectSudokuFieldLineNumberException {
+        if (lineNum < 1 || lineNum > 9)
+            throw new IncorrectSudokuFieldLineNumberException("Line value should be in range [1,9] (now " + lineNum + ")");
+        SudokuPoint[] line = this.field[lineNum - 1];
+        return getNonAvailableValuesForArray(line);
+    }
+
+    private List<Integer> getNonAvailableValuesForHorizontal(int lineNum) throws IncorrectSudokuFieldLineNumberException {
+        if (lineNum < 1 || lineNum > 9)
+            throw new IncorrectSudokuFieldLineNumberException("Line value should be in range [1,9] (now " + lineNum + ")");
+        SudokuPoint[] line = new SudokuPoint[9];
+        for (int i = 0; i < 9; i++) {
+            line[i] = field[i][lineNum - 1];
+        }
+        return getNonAvailableValuesForArray(line);
+    }
+
+    private List<Integer> getNonAvailableValuesForSquare(int squareNum) throws IncorrectSudokuFieldLineNumberException {
+        if (squareNum < 1 || squareNum > 9)
+            throw new IncorrectSudokuFieldLineNumberException("Square value should be in range [1,9] (now " + squareNum + ")");
+        SudokuPoint[] line = new SudokuPoint[9];
+        int x;
+        int y;
+        int mod = squareNum % 3;
+        if (mod == 0) {
+            x = 6;
+        } else if (mod == 1) {
+            x = 0;
+        } else {
+            x = 3;
+        }
+        if (squareNum <= 3) {
+            y = 0;
+        } else if (squareNum <= 6) {
+            y = 3;
+        } else {
+            y = 6;
+        }
+        int lineIndex = 0;
+        for (int i = y; i < y + 3; i++) {
+            for (int j = x; j < x + 3; j++) {
+                line[lineIndex++] = field[j][i];
+            }
+        }
+        return getNonAvailableValuesForArray(line);
+    }
+
+    private List<Integer> getNonAvailableValuesForArray(SudokuPoint[] array) {
+        StringBuilder sb = new StringBuilder();
+        for (SudokuPoint point : array) {
+            sb.append(point.getValue() + " ");
+        }
+        List<Integer> nonAvailableValues = new ArrayList<>();
+        LOG.debug("Getting available values for array: " + sb.toString());
+        for (int i = 0; i < 9; i++) {
+            int currentValue = array[i].getValue();
+            if (currentValue > 0) {
+                nonAvailableValues.add(currentValue);
+            }
+        }
+        LOG.debug("Non available: " + nonAvailableValues);
+        return nonAvailableValues;
     }
 
     private boolean checkArrayToUnique(SudokuPoint[] array) {
